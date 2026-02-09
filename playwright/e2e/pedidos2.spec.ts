0b1705d3a074c3fdeab5c3a6f227ffa455d71654
Geovani Romano <geovaniromano@hotmail.com>
2026-02-09 13:37:58 -0300

    validação de snapshots + ler o trace

--- playwright/e2e/pedidos2.spec.ts
+++ playwright/e2e/pedidos2.spec.ts
@@ -41,12 +41,42 @@ test.describe('Consulta de Pedido', () => {
         await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
         // Assert
-        const containerPedido = page.getByRole('paragraph')
-            .filter({ hasText: /^Pedido$/ }) //expressão regular
-            .locator('..') //sobe para o elemento pai (a div que agrupa ambos)
-        await expect(containerPedido).toContainText(order, { timeout: 10000 })
+        // const containerPedido = page.getByRole('paragraph')
+        //     .filter({ hasText: /^Pedido$/ }) //expressão regular
+        //     .locator('..') //sobe para o elemento pai (a div que agrupa ambos)
+        // await expect(containerPedido).toContainText(order, { timeout: 10000 })
 
-        await expect(page.getByText('APROVADO')).toBeVisible()
+        // await expect(page.getByText('APROVADO')).toBeVisible()
+
+
+        await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order}
+            - img
+            - text: APROVADO
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Lunar White
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: aero Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Geovani Romano
+            - paragraph: Email
+            - paragraph: geovaniromano@hotmail.com
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
 
     })
 
