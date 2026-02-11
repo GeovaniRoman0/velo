49654658d67d3b7a9fbdba19803428f16b1f3efd
Geovani Romano <geovaniromano@hotmail.com>
2026-02-11 09:45:03 -0300

    Criação do teste de consulta de pedido em analise

--- playwright/e2e/pedidos2.spec.ts
+++ playwright/e2e/pedidos2.spec.ts
@@ -178,5 +178,65 @@ test.describe('Consulta de Pedido', () => {
 
     })
 
+    test('deve consultar um pedido em análise', async ({ page }) => {
+
+        //Teste Data
+        //const order = 'VLO-DICR8X'
+        const order = {
+            number: 'VLO-B5ONHA',
+            color: 'Glacier Blue',
+            status:'EM_ANALISE',
+            wheels: 'aero Wheels',
+            customer: {
+                name: 'Rosi Romano',
+                email: 'rosiromano@hotmail.com'
+            },
+            payment: 'À Vista'
+        }
+
+        // Act
+        await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+        // Assert
+        // const containerPedido = page.getByRole('paragraph')
+        //     .filter({ hasText: /^Pedido$/ }) //expressão regular
+        //     .locator('..') //sobe para o elemento pai (a div que agrupa ambos)
+        // await expect(containerPedido).toContainText(order, { timeout: 10000 })
+
+        // await expect(page.getByText('APROVADO')).toBeVisible()
+
+
+        await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order.number}
+            - img
+            - text: ${order.status}
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: ${order.color}
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: ${order.wheels}
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: ${order.customer.name}
+            - paragraph: Email
+            - paragraph: ${order.customer.email}
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: ${order.payment}
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+
+    })
+
 })
 
